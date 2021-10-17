640172752058aefc0347192cca950690bee2b7d6
Akpotor <akpotororodje@gmail.com>
2021-10-18 00:11:25 +0100

    post

--- About_we.js
+++ About_we.js
@@ -1,4 +1,4 @@
-const toTop = document.querySelector(".back_to_top");
+var toTop = document.querySelector(".back_to_top");
 window.addEventListener("scroll", ()=>{
 if (window.pageYOffset > 100) {
     toTop.classList.add("active");
