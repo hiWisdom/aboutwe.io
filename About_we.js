64390e041f8c27673a30bfd610d4d698fef258c3
Akpotor <akpotororodje@gmail.com>
2021-10-23 23:17:00 +0100

    post

--- About_we.js
+++ About_we.js
@@ -1,10 +1,4 @@
-var toTop = document.querySelector(".back_to_top");
-window.addEventListener("scroll", ()=>{
-if (window.pageYOffset > 100) {
-    toTop.classList.add("active");
-} else {
-    toTop.classList.remove("active");
+
+function subscribeBack(){
+window.location="index.html";
 }
-
-
-})
